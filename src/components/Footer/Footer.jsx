2a63016ee909d09e1765b38dfc16a0396420091b
vicky7654dfv <vignesht2013@gmail.com>
2025-10-09 22:58:59 +0530

    footer links path updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,90 +1,171 @@
-import React, { useContext } from 'react'
-import Style from './Footer.module.css'
-import Img1 from "../../assets/Header/logo1.png"
-import { Link } from 'react-router-dom'
-import { ThemeContext } from '../ThemeContext/ThemeContext'
-import AboutUs from './../../pages/AboutUs/AboutUs';
+import React, { useContext } from "react";
+import Style from "./Footer.module.css";
+import Img1 from "../../assets/Header/logo1.png";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../ThemeContext/ThemeContext";
+import AboutUs from "./../../pages/AboutUs/AboutUs";
 export default function Footer() {
-      const {theme} = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-    <hr />
-    <div className={Style.footerWrap} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>
-      <div className={Style.box}>
-        <img src={Img1} alt="" />
-        <p>To redefine modern fashion by blending creativity, comfort, and sustainability — making Stackly a global symbol of confidence and style.</p>
-        <Link to={"/AboutUs"}><button>More about company<i className="fa-solid fa-angles-right"></i></button></Link>
-      </div>
-      <div className={Style.box} >
-        <h4>Quick links</h4>
-        <ul>
-          <li><Link style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>About Us</Link></li>
-          <li><Link style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>Services</Link></li>
-          <li><Link style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>FAQ</Link></li>
-          <li><Link style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>Contact</Link></li>
-          <li><Link style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>Profile</Link></li>
-        </ul>
-      </div>
-      <div className={Style.box}>
-        <h4>Headquarters</h4>
-        <ul  style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>
-          <li><i className="fa-solid fa-location-dot"></i> Chinna tirupathi, Salem</li>
-          <li><i className="fa-solid fa-phone"></i> +91 9876543210</li>
-          <li><i className="fa-solid fa-envelope"></i> info@thestackly.com</li>
-          <li><i className="fa-solid fa-calendar-days"></i> Mon - Fri: 09:30 AM - 06:30 PM</li>
-        </ul>
-      </div>
-      <div className={Style.box}>
-        <h4>Follow Us On</h4>
-        <p>Stay connected with Stackly for the latest fashion trends, style updates, and tips to elevate your wardrobe with confidence.</p>
-        <div className={Style.media} >
-          <Link to={"/Error"} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}><i className="fa-brands fa-linkedin-in"></i></Link>
-          <Link to={"/Error"} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}><i className="fa-brands fa-facebook-f"></i></Link>
-          <Link to={"/Error"} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}><i className="fa-brands fa-x-twitter"></i></Link>
-          <Link to={"/Error"} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}><i className="fa-brands fa-instagram"></i></Link>
+      <hr />
+      <div
+        className={Style.footerWrap}
+        style={{
+          background: theme === "light" ? "#ebebebff" : "#474747ff",
+          color: theme === "light" ? "#474747ff" : "#ebebebff",
+        }}
+      >
+        <div className={Style.box}>
+          <img src={Img1} alt="" />
+          <p>
+            To redefine modern fashion by blending creativity, comfort, and
+            sustainability — making Stackly a global symbol of confidence and
+            style.
+          </p>
+          <Link to={"/AboutUs"}>
+            <button>
+              More about company<i className="fa-solid fa-angles-right"></i>
+            </button>
+          </Link>
+        </div>
+        <div className={Style.box}>
+          <h4>Quick links</h4>
+          <ul>
+            <li>
+              <Link
+                to={"/AboutUs"}
+                style={{
+                  background: theme === "light" ? "#ebebebff" : "#474747ff",
+                  color: theme === "light" ? "#474747ff" : "#ebebebff",
+                }}
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/Services"}
+                style={{
+                  background: theme === "light" ? "#ebebebff" : "#474747ff",
+                  color: theme === "light" ? "#474747ff" : "#ebebebff",
+                }}
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to={"/FAQ"}
+                style={{
+                  background: theme === "light" ? "#ebebebff" : "#474747ff",
+                  color: theme === "light" ? "#474747ff" : "#ebebebff",
+                }}
+              >
+                FAQ
+              </Link>
+            </li>
+            <li>
+              <Link to={"/Contact"}
+                style={{
+                  background: theme === "light" ? "#ebebebff" : "#474747ff",
+                  color: theme === "light" ? "#474747ff" : "#ebebebff",
+                }}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to={"/SignUp"}
+                style={{
+                  background: theme === "light" ? "#ebebebff" : "#474747ff",
+                  color: theme === "light" ? "#474747ff" : "#ebebebff",
+                }}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={Style.box}>
+          <h4>Headquarters</h4>
+          <ul
+            style={{
+              background: theme === "light" ? "#ebebebff" : "#474747ff",
+              color: theme === "light" ? "#474747ff" : "#ebebebff",
+            }}
+          >
+            <li>
+              <i className="fa-solid fa-location-dot"></i> Chinna tirupathi,
+              Salem
+            </li>
+            <li>
+              <i className="fa-solid fa-phone"></i> +91 9876543210
+            </li>
+            <li>
+              <i className="fa-solid fa-envelope"></i> info@thestackly.com
+            </li>
+            <li>
+              <i className="fa-solid fa-calendar-days"></i> Mon - Fri: 09:30 AM
+              - 06:30 PM
+            </li>
+          </ul>
+        </div>
+        <div className={Style.box}>
+          <h4>Follow Us On</h4>
+          <p>
+            Stay connected with Stackly for the latest fashion trends, style
+            updates, and tips to elevate your wardrobe with confidence.
+          </p>
+          <div className={Style.media}>
+            <Link
+              to={"/Error"}
+              style={{
+                background: theme === "light" ? "#ebebebff" : "#474747ff",
+                color: theme === "light" ? "#474747ff" : "#ebebebff",
+              }}
+            >
+              <i className="fa-brands fa-linkedin-in"></i>
+            </Link>
+            <Link
+              to={"/Error"}
+              style={{
+                background: theme === "light" ? "#ebebebff" : "#474747ff",
+                color: theme === "light" ? "#474747ff" : "#ebebebff",
+              }}
+            >
+              <i className="fa-brands fa-facebook-f"></i>
+            </Link>
+            <Link
+              to={"/Error"}
+              style={{
+                background: theme === "light" ? "#ebebebff" : "#474747ff",
+                color: theme === "light" ? "#474747ff" : "#ebebebff",
+              }}
+            >
+              <i className="fa-brands fa-x-twitter"></i>
+            </Link>
+            <Link
+              to={"/Error"}
+              style={{
+                background: theme === "light" ? "#ebebebff" : "#474747ff",
+                color: theme === "light" ? "#474747ff" : "#ebebebff",
+              }}
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-    <div className={Style.bottomFooter} style={{
-        background: theme === "light" ? "#ebebebff" : "#474747ff",
-        color: theme === "light" ? "#474747ff" : "#ebebebff",
-      }}>
-      <hr />
-      <p>Copyright © 2023. Stackly Clothing Company. All Rights Reserved.</p>
-    </div>
-</>
-  )
+      <div
+        className={Style.bottomFooter}
+        style={{
+          background: theme === "light" ? "#ebebebff" : "#474747ff",
+          color: theme === "light" ? "#474747ff" : "#ebebebff",
+        }}
+      >
+        <hr />
+        <p>Copyright © 2023. Stackly Clothing Company. All Rights Reserved.</p>
+      </div>
+    </>
+  );
 }
